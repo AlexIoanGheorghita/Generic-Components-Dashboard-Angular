639cedb518fcd4cb575070ded906386242758760
Alexandru Gheorghita <alexandru.gheorghita@bearingpoint.com>
2022-12-12 11:31:19 +0200

    Created a shared module

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from "@angular/core";
+import { GenericDialogModule } from "./generic-dialog/generic-dialog.module";
+
+@NgModule({
+  imports: [
+    GenericDialogModule
+  ],
+  declarations: [],
+  exports: [
+    GenericDialogModule
+  ]
+})
+export class SharedModule {
+
+}
