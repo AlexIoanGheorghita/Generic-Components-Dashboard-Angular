b4e7d2e52eb25ecc293e37f07b93126c8da0cf51
Alexandru Gheorghita <alexandru.gheorghita@bearingpoint.com>
2022-12-12 15:28:46 +0200

    Changed app component to display header based on route url

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { AuthService } from './shared/auth/auth.service';
+import { HeaderService } from './shared/header/services/header.service';
 
 @Component({
   selector: 'app-root',
@@ -8,9 +11,17 @@ import { AuthService } from './shared/auth/auth.service';
 })
 export class AppComponent implements OnInit {
   title = 'AngularExercise';
-  constructor(private authService: AuthService) {}
+  currentPath: string;
+
+  constructor(
+    private authService: AuthService,
+    private location: Location,
+    private headerService: HeaderService
+   ) {}
 
   ngOnInit() {
     this.authService.autoLogin();
+
+    this.currentPath = this.location.path();
   }
 }
