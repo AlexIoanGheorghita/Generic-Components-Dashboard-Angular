7914828c5eec7d1c82285b076b3d686d640ddbca
Alexandru Gheorghita <alexandru.gheorghita@bearingpoint.com>
2022-12-12 11:33:51 +0200

    Modified login component to display error messages

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatFormFieldControl } from '@angular/material/form-field';
+import { AuthService } from 'src/app/shared/auth/auth.service';
+import { LoginError } from 'src/app/shared/auth/user.model';
 
 @Component({
   selector: 'app-login',
@@ -9,14 +11,23 @@ import { MatFormFieldControl } from '@angular/material/form-field';
 })
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
+  errorMessage: LoginError | null;
 
-  constructor() {}
+  constructor(private authService: AuthService) {}
 
   ngOnInit() {
     this.loginForm = new FormGroup({
       'email': new FormControl(null, [Validators.email, Validators.required]),
       'password': new FormControl(null, [Validators.minLength(8), Validators.required])
+    });
+
+    this.authService.loginError.subscribe(error => {
+      this.errorMessage = error;
     })
+  }
+
+  login(credentials: { email: string, password: string }): void {
+    this.authService.login(credentials);
   }
 
   onSubmit(): void {
