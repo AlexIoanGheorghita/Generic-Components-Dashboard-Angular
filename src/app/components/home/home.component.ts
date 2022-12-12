a69ba870803ffc55d5fe1b12a7b2305f6512e522
Alexandru Gheorghita <alexandru.gheorghita@bearingpoint.com>
2022-12-12 15:29:25 +0200

    Moved header to app component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HeaderService } from '../../shared/header/services/header.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  constructor(private headerService: HeaderService) {}
+
+  ngOnInit(): void {
+    this.headerService.title.next('Home');
+  }
 }
