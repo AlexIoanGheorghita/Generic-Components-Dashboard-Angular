184d6fcacace38c2e979766cb810ac82de3cf17a
Alexandru Gheorghita <alexandru.gheorghita@bearingpoint.com>
2022-12-15 14:06:28 +0200

    Modified shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,32 @@
 import { NgModule } from "@angular/core";
 import { GenericDialogModule } from "./generic-dialog/generic-dialog.module";
+import { MatTableModule } from '@angular/material/table';
 import { HeaderComponent } from "./header/header.component";
+import { GenericInputComponent } from './generic-input/generic-input.component';
+import { GenericTableComponent } from './generic-table/generic-table.component';
+import { MatPaginatorModule } from "@angular/material/paginator";
+import { HttpClientModule } from "@angular/common/http";
+import { CommonModule } from "@angular/common";
+import { MatSortModule } from "@angular/material/sort";
 
 @NgModule({
   imports: [
-    GenericDialogModule
+    GenericDialogModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    HttpClientModule,
+    CommonModule
   ],
-  declarations: [HeaderComponent],
+  declarations: [HeaderComponent, GenericInputComponent, GenericTableComponent],
   exports: [
     GenericDialogModule,
-    HeaderComponent
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    HttpClientModule,
+    HeaderComponent,
+    GenericTableComponent
   ]
 })
 export class SharedModule {
