ab69110ca53c98f1b2cb020106af98eb2740afdc
Alexandru Gheorghita <alexandru.gheorghita@bearingpoint.com>
2022-12-15 14:05:56 +0200

    Added the generic table to the home page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { StarWarsService } from 'src/app/shared/services/star-wars.service';
 import { HeaderService } from '../../shared/header/services/header.service';
 
 @Component({
@@ -6,11 +8,28 @@ import { HeaderService } from '../../shared/header/services/header.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  data: any;
+  columns: string[];
+  private subscription: Subscription;
 
-  constructor(private headerService: HeaderService) {}
+  constructor(
+    private headerService: HeaderService,
+    private starWarsService: StarWarsService
+  ) {}
 
   ngOnInit(): void {
     this.headerService.title.next('Home');
+
+    this.subscription = this.starWarsService.init('/people').subscribe(items => {
+      this.starWarsService.setItems(items['results']);
+      this.data = this.starWarsService.getItems();
+      console.log(this.data);
+      this.columns = Object.keys(this.starWarsService.getItem(1)).slice(1,6);
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
