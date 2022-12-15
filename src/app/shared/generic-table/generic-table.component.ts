e345ead92f3b586d372b8c6b2c0047b555c2b1fd
Alexandru Gheorghita <alexandru.gheorghita@bearingpoint.com>
2022-12-15 11:21:39 +0200

    Modified the generic table

--- src/app/shared/generic-table/generic-table.component.ts
+++ src/app/shared/generic-table/generic-table.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, Input, OnInit, ViewChild } from '@angular/core';
+import { Component, Input, ViewChild } from '@angular/core';
 import { MatSort } from '@angular/material/sort';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
@@ -8,23 +8,24 @@ import { MatTableDataSource } from '@angular/material/table';
   templateUrl: './generic-table.component.html',
   styleUrls: ['./generic-table.component.scss']
 })
-export class GenericTableComponent implements OnInit, AfterViewInit {
-  @Input() tableData: any[];
-  @Input() columns: string[];
+export class GenericTableComponent {
+  @Input() set tableData(data: any[]) {
+    this.dataSource = new MatTableDataSource(data);
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+  };
+  @Input() set columns(columnData: string[]) {
+    this.newColumns = columnData?.slice().map(column => {
+      let columnArray = column.split('');
+      columnArray = [columnArray[0].toUpperCase(), ...columnArray.slice(1)];
+      return columnArray.join('');
+    });
+  };
   @Input() service: any;
   @ViewChild(MatSort) sort: MatSort;
   @ViewChild(MatPaginator) paginator: MatPaginator;
-
+  newColumns: string[];
   dataSource: MatTableDataSource<any>;
 
   constructor() {}
-
-  ngOnInit() {
-    this.dataSource = new MatTableDataSource(this.tableData);
-  }
-
-  ngAfterViewInit() {
-    this.dataSource.sort = this.sort;
-    this.dataSource.paginator = this.paginator;
-  }
 }
