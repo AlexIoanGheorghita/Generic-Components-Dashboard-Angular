a58aaa50b35b6d17264e21aa21d0831c90748034
Alexandru Gheorghita <alexandru.gheorghita@bearingpoint.com>
2022-12-23 15:47:02 +0200

    Added add-item component to the routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddItemComponent } from './components/add-item/add-item.component';
 import { DetailsComponent } from './components/details/details.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
@@ -8,6 +9,7 @@ import { AuthGuard } from './shared/auth/auth.guard';
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full', canActivate: [AuthGuard] },
   { path: 'items/:item_id', component: DetailsComponent, canActivate: [AuthGuard] },
+  { path: 'item/add', component: AddItemComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent }
 ];
 
