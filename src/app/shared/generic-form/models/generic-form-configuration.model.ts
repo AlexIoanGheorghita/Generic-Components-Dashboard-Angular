90f4834c95c5a5ccfe918a5778bd59ee8abda475
Alexandru Gheorghita <alexandru.gheorghita@bearingpoint.com>
2022-12-23 15:43:50 +0200

    Modified the generic form configuration model

--- src/app/shared/generic-form/models/generic-form-configuration.model.ts
+++ src/app/shared/generic-form/models/generic-form-configuration.model.ts
@@ -1,8 +1,10 @@
 import { FormGroup } from "@angular/forms";
+import { Button } from "../../generic-button/models/button.model";
 import { GenericFormField } from "../../models/input.model";
 
 export interface GenericFormConfiguration {
   formId: string,
   formGroup: FormGroup,
-  formFields: GenericFormField[]
+  formFields: GenericFormField[],
+  formActions: Button[]
 }
