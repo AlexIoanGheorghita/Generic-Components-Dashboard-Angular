1bdc85ed0efd91ce6bd08cd0ba935dec027691f8
Alexandru Gheorghita <alexandru.gheorghita@bearingpoint.com>
2022-12-23 15:42:10 +0200

    Modified generic select component

--- src/app/shared/generic-select/generic-select.component.ts
+++ src/app/shared/generic-select/generic-select.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
+import { FormControl, FormGroupDirective } from '@angular/forms';
+import { distinctUntilChanged, Subscription } from 'rxjs';
 import { GenericFormField } from '../models/input.model';
 
 @Component({
@@ -6,6 +8,33 @@ import { GenericFormField } from '../models/input.model';
   templateUrl: './generic-select.component.html',
   styleUrls: ['./generic-select.component.scss']
 })
-export class GenericSelectComponent {
+export class GenericSelectComponent implements OnInit, OnDestroy {
   @Input() selectConfig: GenericFormField;
+  control: FormControl;
+  isMultiple: boolean = false;
+  errorMessage: string| undefined;
+  private statusSub: Subscription;
+
+  constructor(private rootFormGroup: FormGroupDirective) {}
+
+  ngOnInit(): void {
+    if (this.selectConfig.fieldDataType === 'checkbox') {
+      this.isMultiple = true;
+    }
+    this.control = this.rootFormGroup.control.get(this.selectConfig.formFieldId) as FormControl;
+
+    this.errorMessage = this.selectConfig.validations.find(item => item.validationType === 'required')?.errorMessage;
+
+    this.statusSub = this.control.statusChanges.pipe(distinctUntilChanged()).subscribe(status => {
+      if (status === 'INVALID') {
+        this.errorMessage = this.selectConfig.validations.find(item => {
+          return item.validationType === Object.keys(this.control.errors!)[0]
+        })?.errorMessage;
+      }
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.statusSub.unsubscribe();
+  }
 }
