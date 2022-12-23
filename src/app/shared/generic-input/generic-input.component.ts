741d68e5a7318fcf0944b23332a56dee0de6ff45
Alexandru Gheorghita <alexandru.gheorghita@bearingpoint.com>
2022-12-23 15:41:12 +0200

    Modified generic-input component

--- src/app/shared/generic-input/generic-input.component.ts
+++ src/app/shared/generic-input/generic-input.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { FormControl, FormGroupDirective } from '@angular/forms';
+import { distinctUntilChanged, Subscription } from 'rxjs';
 import { GenericFormField } from '../models/input.model';
 
 @Component({
@@ -6,6 +8,28 @@ import { GenericFormField } from '../models/input.model';
   templateUrl: './generic-input.component.html',
   styleUrls: ['./generic-input.component.scss']
 })
-export class GenericInputComponent {
+export class GenericInputComponent implements OnInit, OnDestroy {
   @Input() inputConfig: GenericFormField;
+  control: FormControl;
+  errorMessage: string | undefined = '';
+  statusSub: Subscription;
+
+  constructor(private rootFormGroup: FormGroupDirective) {}
+
+  ngOnInit(): void {
+    this.control = this.rootFormGroup.control.get(this.inputConfig.formFieldId) as FormControl;
+    this.errorMessage = this.inputConfig.validations.find(item => item.validationType === 'required')?.errorMessage;
+
+    this.statusSub = this.control.statusChanges.pipe(distinctUntilChanged()).subscribe(status => {
+      if (status === 'INVALID') {
+        this.errorMessage = this.inputConfig.validations.find(item => {
+          return item.validationType === Object.keys(this.control.errors!)[0]
+        })?.errorMessage;
+      }
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.statusSub.unsubscribe();
+  }
 }
