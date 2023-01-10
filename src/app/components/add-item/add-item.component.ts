db88740ac290999f6b248165f2363ed8087a0042
Alexandru Gheorghita <alexandru.gheorghita@bearingpoint.com>
2023-01-10 12:37:49 +0200

    Made the add item page to work

--- src/app/components/add-item/add-item.component.ts
+++ src/app/components/add-item/add-item.component.ts
@@ -1,41 +1,43 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
 import { GenericFormConfiguration } from 'src/app/shared/generic-form/models/generic-form-configuration.model';
-import { InputDataTypes } from 'src/app/shared/models/input-data-types.enum';
-import { FormFieldTypeEnum } from 'src/app/shared/models/input-types.enum';
 import { FORM_CONFIGURATION } from 'src/app/shared/models/form-configuration.const';
-
-// export const FORM_CONFIGURATION: GenericFormConfiguration = {
-//   formId: 'add-item-form',
-//   formGroup: new FormGroup({}),
-//   formFields: [
-//     {
-//       formFieldId: 'name',
-//       formControl: new FormControl('', [Validators.required]),
-//       title: 'Name',
-//       placeholder: 'Enter name',
-//       value: '',
-//       required: true,
-//       disabled: false,
-//       validations: [
-//         {
-//           validationType: 'required',
-//           errorMessage: 'Name field is required'
-//         }
-//       ],
-//       fieldType: FormFieldTypeEnum.INPUT,
-//       fieldDataType: InputDataTypes.TEXT
-//     }
-//   ]
-// }
+import { StarWarsService } from 'src/app/shared/services/star-wars.service';
+import { HeaderService } from 'src/app/shared/header/services/header.service';
+import { Person } from 'src/app/shared/models/star-wars-person.model';
 
 @Component({
   selector: 'app-add-item',
   templateUrl: './add-item.component.html',
   styleUrls: ['./add-item.component.scss']
 })
-export class AddItemComponent {
+export class AddItemComponent implements OnInit{
   formConfig: GenericFormConfiguration = FORM_CONFIGURATION;
 
-  constructor() {}
+  constructor(
+    private starWarsService: StarWarsService,
+    private headerService: HeaderService
+  ) {}
+
+  ngOnInit(): void {
+    this.headerService.title.next('Add a person');
+
+    this.formConfig.formActions = [
+      {
+        text: 'Add Person',
+        config: {
+          color: '#FFFFFF',
+          backgroundColor: '#4d4dff',
+          fontSize: 16,
+          borderRadius: 5
+        },
+        action: () => {
+          let newPerson = this.formConfig.formGroup.value;
+
+          const correctBirthYear = new Date(newPerson.birth_year).getFullYear();  // convert date + timezone into year
+          newPerson = { ...newPerson, birth_year: correctBirthYear, height: +newPerson.height, mass: +newPerson.mass };
+          this.starWarsService.addItem(newPerson as Person);
+        }
+      }
+    ]
+  }
 }
