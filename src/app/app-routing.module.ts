d4b58dff34db8a3f851aaa31d9fa612ea8187010
Alexandru Gheorghita <alexandru.gheorghita@bearingpoint.com>
2023-01-11 15:31:24 +0200

    Added edit-item page route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddItemComponent } from './components/add-item/add-item.component';
 import { DetailsComponent } from './components/details/details.component';
+import { EditItemComponent } from './components/edit-item/edit-item.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { AuthGuard } from './shared/auth/auth.guard';
@@ -10,6 +11,7 @@ const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full', canActivate: [AuthGuard] },
   { path: 'items/:item_id', component: DetailsComponent, canActivate: [AuthGuard] },
   { path: 'item/add', component: AddItemComponent, canActivate: [AuthGuard] },
+  { path: 'items/:item_id/edit', component: EditItemComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent }
 ];
 
