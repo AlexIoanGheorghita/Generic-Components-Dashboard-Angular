092676249e4190d9e39286c633f505a1bdfa9f5d
Alexandru Gheorghita <alexandru.gheorghita@bearingpoint.com>
2023-01-11 15:30:58 +0200

    Added logic to edit item page

--- src/app/components/edit-item/edit-item.component.ts
+++ src/app/components/edit-item/edit-item.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { GenericFormConfiguration } from 'src/app/shared/generic-form/models/generic-form-configuration.model';
+import { HeaderService } from 'src/app/shared/header/services/header.service';
+import { FORM_CONFIGURATION } from 'src/app/shared/models/form-configuration.const';
+import { Person } from 'src/app/shared/models/star-wars-person.model';
+import { StarWarsService } from 'src/app/shared/services/star-wars.service';
 
 @Component({
   selector: 'app-edit-item',
   templateUrl: './edit-item.component.html',
   styleUrls: ['./edit-item.component.scss']
 })
-export class EditItemComponent {
+export class EditItemComponent implements OnInit, OnDestroy {
+  formConfig: GenericFormConfiguration = FORM_CONFIGURATION;
+  person: Person | undefined;
+  errorMessage: string;
+  private routeSub: Subscription;
 
+  constructor(
+    private starWarsService: StarWarsService,
+    private headerService: HeaderService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.routeSub = this.route.params.subscribe((params: Params) => {
+      if (!isNaN(+params['item_id'])) {
+        const result = this.starWarsService.getItem(+params['item_id']);
+        if (result !== undefined) {
+          this.person = result;
+        } else {
+          this.errorMessage = 'This is not the record you are looking for';
+        }
+      } else {
+        this.errorMessage = 'You have to search for a record using an ID';
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.routeSub.unsubscribe();
+  }
 }
