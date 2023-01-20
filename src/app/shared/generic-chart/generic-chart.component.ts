0872a2cd0abdbd54a005525d5d48595485d3f1c3
Alexandru Gheorghita <alexandru.gheorghita@bearingpoint.com>
2023-01-20 15:17:28 +0200

    Modified chart component and styling

--- src/app/shared/generic-chart/generic-chart.component.ts
+++ src/app/shared/generic-chart/generic-chart.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, Input, OnInit, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
+import { ChartEvent } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
+import { ChartConfig } from './models/chart-config.model';
+
+type ChartEventType = { event: ChartEvent, active: {}[] };
 
 @Component({
   selector: 'app-generic-chart',
   templateUrl: './generic-chart.component.html',
-  styleUrls: ['./generic-chart.component.scss']
+  styleUrls: ['./generic-chart.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class GenericChartComponent {
+export class GenericChartComponent implements OnInit {
+  @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
+  @Input() chartConfig: ChartConfig;
 
+  constructor(private changeDetectorRef: ChangeDetectorRef) {}
+
+  ngOnInit(): void {
+    console.log(this.chartConfig);
+    this.changeDetectorRef.detectChanges();
+  }
+
+  chartClicked({ event, active }: ChartEventType) {
+    console.log(event, active);
+  }
+
+  chartHovered({ event, active }: ChartEventType) {
+    console.log(event, active);
+  }
 }
