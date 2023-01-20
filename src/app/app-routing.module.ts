c4aab586586a8e9691065b0af10432b56bee6cc0
Alexandru Gheorghita <alexandru.gheorghita@bearingpoint.com>
2023-01-20 15:22:19 +0200

    Added admin guard to appropriate routes in the routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,13 +7,14 @@ import { SaveChangesGuard } from './components/edit-item/save-changes.guard';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
+import { AdminGuard } from './shared/auth/admin.guard';
 import { AuthGuard } from './shared/auth/auth.guard';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full', canActivate: [AuthGuard] },
   { path: 'items/:item_id', component: DetailsComponent, canActivate: [AuthGuard] },
-  { path: 'item/add', component: AddItemComponent, canActivate: [AuthGuard] },
-  { path: 'items/:item_id/edit', component: EditItemComponent, canActivate: [AuthGuard], canDeactivate: [SaveChangesGuard] },
+  { path: 'item/add', component: AddItemComponent, canActivate: [AdminGuard] },
+  { path: 'items/:item_id/edit', component: EditItemComponent, canActivate: [AdminGuard], canDeactivate: [SaveChangesGuard] },
   { path: 'login', component: LoginComponent },
   { path: '**', component: NotFoundComponent }
 ];
