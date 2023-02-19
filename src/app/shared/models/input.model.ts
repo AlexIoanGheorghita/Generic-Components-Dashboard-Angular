43254ba15d0eb27df30bacd5c84ff99041b1ebb7
Alexandru Gheorghita <alexandru.gheorghita@bearingpoint.com>
2023-02-19 15:00:28 +0200

    Added more properties to the generic field model

--- src/app/shared/models/input.model.ts
+++ src/app/shared/models/input.model.ts
@@ -1,4 +1,6 @@
-import { FormControl, ValidatorFn } from "@angular/forms";
+import { FormArray, FormControl, FormGroup, ValidatorFn } from "@angular/forms";
+import { Button } from "../generic-button/models/button.model";
+import { FormControlType } from "../generic-form/models/form-control-type.enum";
 import { InputDataTypes } from "./input-data-types.enum";
 import { FormFieldTypeEnum } from "./input-types.enum";
 
@@ -13,19 +15,22 @@ import { FormFieldTypeEnum } from "./input-types.enum";
 
 export interface GenericFormField {
   formFieldId: string,
-  formControl: FormControl,
-  title: string,
-  placeholder: string,
+  formControl: FormControl | FormGroup | FormArray,
+  formControlType: FormControlType,
+  formFields?: GenericFormField[],
+  title?: string,
+  placeholder?: string,
   value?: string | number | string[],
-  required: boolean,
-  disabled: boolean,
-  validations: FormFieldValidation[],
-  fieldType: FormFieldTypeEnum,
+  required?: boolean,
+  disabled?: boolean,
+  validations?: FormFieldValidation[],
+  fieldType?: FormFieldTypeEnum,
   fieldDataType?: InputDataTypes,
   selectItems?: {
     label: string,
     value: string
-  }[]
+  }[],
+  formControlActions?: Button[]
 }
 
 export interface FormFieldValidation {
